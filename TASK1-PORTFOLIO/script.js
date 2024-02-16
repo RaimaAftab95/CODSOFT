c6f04fbb58f1a839bfc34776c3c7c24e6e498a86
Raima Aftab <raima.aftab2022@gmail.com>
2024-02-16 17:45:19 +0500

    TASK1
    Portfolio

---
+++ TASK1-PORTFOLIO/script.js
@@ -0,0 +1,6 @@
+//   var typed = new Typed('#typed-text', {
+//     strings: ['Designer', 'Developer', 'Freelancer', 'Photographer'],
+//     typeSpeed: 50, // typing speed in milliseconds
+//     backSpeed: 50, // backspacing speed in milliseconds
+//     loop: true // loop infinitely
+//   });
