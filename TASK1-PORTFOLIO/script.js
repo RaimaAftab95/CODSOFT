8ad6900884b20aa7793bb5b30235594607f890e5
Raima Aftab <raima.aftab2022@gmail.com>
2024-03-02 14:46:48 +0500

    animation added

--- TASK1-PORTFOLIO/script.js
+++ TASK1-PORTFOLIO/script.js
@@ -4,3 +4,22 @@
 //     backSpeed: 50, // backspacing speed in milliseconds
 //     loop: true // loop infinitely
 //   });
+// ********scroll to top********
+// Get the scroll-to-top element
+const scrollToTop = document.getElementById('scroll-to-top');
+
+// Get the section2 element
+const section2 = document.getElementById('About');
+
+// Check if the scroll position is below the section1
+window.addEventListener('scroll', function () {
+  const section1OffsetTop = section2.offsetTop;
+  const scrollPosition = window.scrollY + window.innerHeight;
+
+  // Display the scroll-to-top icon only if the scroll position is below section1
+  if (scrollPosition >= section1OffsetTop) {
+    scrollToTop.style.display = 'block';
+  } else {
+    scrollToTop.style.display = 'none';
+  }
+});
